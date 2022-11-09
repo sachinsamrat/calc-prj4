aae8ded5300b2d38f345ef4627e90d856ee278bd
nagapavan <praveen@fournine.com>
2022-11-09 06:54:26 +0530

    menu for colors and logic for exercise

---
+++ src/Calculator/Exercise/ExerciseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Exercise
+{
+    public class ExerciseDTO
+    {
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> Options { get; set; }
+    }
+}
---
+++ src/Calculator/Menu.xaml.cs
@@ -0,0 +1,49 @@
+namespace Calculator;
+
+public partial class Menu : ContentPage
+{
+    public Menu()
+    {
+        InitializeComponent();
+    }
+
+    void onlightclick(object sender, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new LightTheme());
+        }
+    }
+
+    void ondarkclick(object sender, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new DarkTheme());
+        }
+    }
+
+    void ongreenclick(object sender, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new GreenTheme());
+        }
+    }
+
+    void onpinkclick(object sender, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new PinkTheme());
+        }
+    }
+}
